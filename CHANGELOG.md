02987f967f518a9bde8fdf458d416177204a7486
Hasan Shahrier <66935697+hasan-shahrier@users.noreply.github.com>
2023-02-25 23:48:16 +0600

    Revert "Release 1.2.0"
    This reverts commit 57150e49e6e7f7bb8013e3f440c96450203566bd.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,36 +1,12 @@
 # Change Log
 
-## Release - 1.1.0
-
-- 2nd Public release.
-
-### Added in 1.1.0
-
-- Bracket Pair Guide colors.
-- A few of Git colors.
-
-### Updated in 1.1.0
-
-- UI's Background and Foreground colors.
-- Symbol Icon colors.
-- Debug Icon colors.
-- Fixed Input Control related colors.
-- Fixed List Hover colors.
-- Styx and Styx+ Syntax Highligthing colors.
-
-### Removed in 1.1.0
-
-- None.
-
-</br>
-
 ## Release - 1.0.0
 
-- Initial release.
+- Initial release
 
-### Added in 1.0.0
+### Added
 
 - Themed UI.
-- 7 Syntax Highlighting Schemes (Styx, Styx+, Styx Dracula, Styx Fleet, Styx Material, Styx Monokai, Styx VSCode).
+- 7 Syntax Highlighting Schemes (Styx, Styx+, Styx Dracula, Styx Fleet, Styx Material, Styx Monokai, Styx VSCode)
 - Syntax Highlighting support for Python, C, C++, Julia, Java, Kotlin (Partial), JavaScript and TypeScript.
 - Syntax Highlighting support for HTML, CSS and Markdown.
--- src/colors.py
+++ src/colors.py
@@ -16,42 +16,42 @@ def get_shade(color, s, v):
 
 @dataclass(frozen=True, init=False)
 class BaseColors:
-    # Background
-    bg0: str = "#000000"                #   HSV -> 0°, 0%, 0%
-    bg1: str = "#101112"                #   HSV -> 225°, 10%, 7%
-    bg2: str = get_shade(bg1, 10, 14)
+    #   Background
+    bg0: str = "#000000"                #   Material - Black
+    bg1: str = "#101010"
+    bg2: str = "#202020"
     
-    # Foreground
-    fg0: str = "#C2C5CC"                #   HSV -> 225°, 5%, 80%
-    fg1: str = get_shade(fg0, 10, 60)
-    fg2: str = get_shade(fg0, 30, 30)
+    #   Foreground
+    fg0: str = "#CFD8DC"                #   Material - Blue Gray 100
+    fg1: str = "#78909C"                #   Material - Blue Gray 400
+    fg2: str = "#2E364D"
     
-    # Primary Accent
+    #   Primary Accent
     pa0: str = "#64FFDA"                #   Material - Teal A200
     pa1: str = get_shade(pa0, 80, 40)
     pa2: str = get_shade(pa0, 80, 20)
     
-    # Secondary Accent
-    sa0: str = "#80C0FF"                #   HSV -> 210°, 50%, 100%
-    sa1: str = get_shade(sa0, 80, 35)
-    sa2: str = get_shade(sa0, 80, 15)
+    #   Secondary Accent
+    sa0: str = "#40C4FF"                #   Material - Light Blue A200
+    sa1: str = get_shade(sa0, 80, 40)
+    sa2: str = get_shade(sa0, 80, 20)
     
-    # Warning Accent
-    wa0: str = "#FFE9A6"                #   HSV -> 45°, 35%, 100%
-    wa1: str = get_shade(wa0, 80, 35)
-    wa2: str = get_shade(wa0, 80, 15)
+    #   Warning Accent
+    wa0: str = "#FFFF00"                #   Material - Yellow A200
+    wa1: str = get_shade(wa0, 80, 40)
+    wa2: str = get_shade(wa0, 80, 20)
     
-    # Debug Accent
-    da0: str = "#BB99FF"                #   HSV -> 240°, 40%, 100%
+    #   Debug Accent
+    da0: str = "#B388FF"                #   Material - Deep Purple A100
     da1: str = get_shade(da0, 60, 30)
     da2: str = get_shade(da0, 60, 10)
     
-    # Error Accent
-    ea0: str = "#FF8080"                #   HSV -> 0°, 50%, 100%
-    ea1: str = get_shade(ea0, 80, 35)
-    ea2: str = get_shade(ea0, 80, 15)
+    #   Error Accent
+    ea0: str = "#FF5252"                #   Material - Red A200
+    ea1: str = get_shade(ea0, 80, 40)
+    ea2: str = get_shade(ea0, 80, 20)
     
-    # Terminal
+    #   Terminal
     ansiBlack: str = bg0                #   Material - Black
     ansiBrightBlack: str = "#757575"    #   Material - Gray 600
     ansiRed: str = "#E81123"            #   Fluent - Red
@@ -69,13 +69,13 @@ class BaseColors:
     ansiWhite: str = "#CFD8DC"          #   Material - Blue Gray 100
     ansiBrightWhite: str = "#FFFFFF"    #   Material - White
     
-    # Debug
+    #   Debug
     red: str = "#FF0000"
 
 
 @dataclass(frozen=True, init=False)
 class DraculaTokenColors:
-    # https://github.com/dracula/visual-studio-code
+    #   https://github.com/dracula/visual-studio-code
     white: str = "#ECEFF1"              #   Material - Blue Gray 50
     gray: str = "#6272A4"               #   Dracula - Gray
     cyan: str = "#8BE9FD"               #   Dracula - Cyan
@@ -106,7 +106,7 @@ class MaterialTokenColors:
 
 @dataclass(frozen=True, init=False)
 class MonokaiTokenColors:
-    # https://monokai.pro/
+    #   https://monokai.pro/
     white: str = "#ECEFF1"              #   Material - Blue Gray 50
     gray: str = "#908E8F"               #   Monokai - Gray
     lightGray: str = "#CCCACA"
@@ -115,7 +115,7 @@ class MonokaiTokenColors:
     orange: str = "#FC9867"             #   Monokai - Orange
     pink: str = "#FF6188"               #   Monokai - Pink
     purple: str = "#AB9DF2"             #   Monokai - Purple
-    red: str = "#FF5555"                #   Dracula - Red
+    # red: str = "#FF5555"              #   Dracula - Red
     yellow: str = "#FFD866"             #   Monokai - Yellow
     
 
@@ -153,7 +153,7 @@ class FleetTokenColors:
 
 @dataclass(frozen=True, init=False)
 class VSCodeDefaultTokenColors:
-    # https://github.com/microsoft/vscode/tree/main/extensions/theme-defaults
+    #   https://github.com/microsoft/vscode/tree/main/extensions/theme-defaults
     white: str = "#C8C8C8"              #   VSC Default
     gray: str = "#808080"               #   VSC Default - Unused
     lightGray: str = "#D4D4D4"          #   VSC Default - Unused
@@ -182,11 +182,11 @@ class StyxTokenColors:
     green: str = "#45E6B0"              #   HSV -> 160°, 70%, 90%
     teal: str = "#8AE6C0"               #   HSV -> 155°, 40%, 90%
     cyan: str = "#91F2F2"               #   HSV -> 180°, 40%, 95%
-    blue: str = "#59d6ff"               #   HSV -> 195°, 65%, 100%
+    blue: str = "#61CEF2"               #   HSV -> 195°, 60%, 95%
     coral: str = "#E67386"              #   HSV -> 350°, 50%, 90%
     pink: str = "#FFB3D9"               #   HSV -> 330°, 30%, 100%
     purple: str = "#AA80FF"             #   HSV -> 260°, 50%, 100%
-    periwinkle: str = "#BFBFFF"         #   HSV -> 240°, 25%, 100%
+    periwinkle: str = "#B6B6F2"         #   HSV -> 240°, 25%, 95%
     red: str = "#F25555"                #   HSV -> 0°, 65%, 95%
     amber: str = "#FFA64D"              #   HSV -> 30°, 70%, 100%
     yellow: str = "#FFE9A6"             #   HSV -> 45°, 35%, 100%
